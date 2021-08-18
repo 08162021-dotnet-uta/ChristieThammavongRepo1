26c8e1f72e61e47b5043674f8c94adcb28a46764
christie thammavong <christie.thammavong@revature.net>
2021-08-18 16:07:30 -0600

    added exception handling for non-numeric values

--- week_1/Calculator/Program.cs
+++ week_1/Calculator/Program.cs
@@ -7,17 +7,18 @@ namespace Calculator
         static void Main(string[] args)
         {
           // input stuff
-          var input1 = int.Parse(Console.ReadLine()); //type inference, parsing
-          var input2 = int.Parse(Console.ReadLine());
-          
-          // compute stuff
-          int result1 = Add(input1, input2);
-          int result2 = Subtract(input1, input2);
-          int result3 = Multiply(input1, input2);
-          int result4 = Divide(input1, input2);
+          // int input1, input2;
+          if(Input(out int input1, out int input2))
+          {
+            // compute stuff
+            int result1 = Add(input1, input2);
+            int result2 = Subtract(input1, input2);
+            int result3 = Multiply(input1, input2);
+            int result4 = Divide(input1, input2);
 
-          // output stuff
-          Print(result1, result2, result3, result4);
+            // output stuff
+            Print(result1, result2, result3, result4);
+          }
         }
 
         static int Add(int input1, int input2)
@@ -60,6 +61,55 @@ namespace Calculator
           {
               Console.WriteLine(result);
           }
+        }
+
+        static bool CustomTryParse(string s, out int result) // TryParse behind-the-scenes
+        {
+          try
+          {
+            result = int.Parse(s);
+            return true;
+          }
+          catch
+          {
+            result = 0;
+            return false;
+          }
+        }
+
+        static bool Input(out int i1, out int i2)
+        {
+          // input stuff
+          int input1, input2;
+
+          if (int.TryParse(Console.ReadLine(), out input1) && int.TryParse(Console.ReadLine(), out input2))
+          {
+            i1 = input1;
+            i2 = input2;
+
+            return true;
+          }
+          else
+          {
+            i1 = i2 = 0;
+
+            return false;
+          }
+          
+          // try
+          // {
+          //   var input1 = int.Parse(Console.ReadLine()); //type inference, parsing
+          //   var input2 = int.Parse(Console.ReadLine());
+          // }
+          // catch(Exception ex)
+          // {
+          //   //throw ex; // points to the original error
+          //   throw new Exception(" ", ex); // create a new error
+          // }
+          // finally
+          // {
+          //   // always run
+          // }
         }
     }
 }
