7110c90037ccd4b5f33c1a9d2d25fd197d4fbc3d
christie thammavong <christie.thammavong@revature.net>
2021-08-19 16:18:08 -0600

    created project_0 solution and projects

---
+++ projects/project_0/Project0.StoreApplication.Domain/Models/Order.cs
@@ -0,0 +1,23 @@
+namespace Project0.StoreApplication.Domain.Models
+{
+  class Order
+  {
+    public Customer Customer
+    {
+      get;
+      set;
+    }
+
+    public Product Product
+    {
+      get;
+      set;
+    }
+
+    public Store Store
+    {
+      get;
+      set;
+    }
+  }
+}
