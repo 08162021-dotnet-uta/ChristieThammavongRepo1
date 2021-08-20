3de85d50fc426def8c2c13affb96f685c13c28de
christie thammavong <christie.thammavong@revature.net>
2021-08-20 15:51:45 -0600

    created bookstore file

---
+++ projects/project_0/Project0.StoreApplication.Domain/Models/BookStore.cs
@@ -0,0 +1,12 @@
+using Project0.StoreApplication.Domain.Abstracts;
+
+namespace Project0.StoreApplication.Domain.Models
+{
+  public class BookStore : Store
+  {
+    public BookStore()
+    {
+      Name = "Bookstore001";
+    }
+  }
+}
