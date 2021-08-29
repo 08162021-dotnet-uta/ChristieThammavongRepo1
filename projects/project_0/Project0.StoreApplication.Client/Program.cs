f6e86354160b3c1594129801081469db0ca9056c
christie thammavong <christie.thammavong@revature.net>
2021-08-28 23:25:49 -0600

    added singletons and modified relevant files

--- projects/project_0/Project0.StoreApplication.Client/Program.cs
+++ projects/project_0/Project0.StoreApplication.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Project0.StoreApplication.Client.Singletons;
 using Project0.StoreApplication.Domain.Models;
 using Project0.StoreApplication.Storage.Repositories;
 using Serilog;
@@ -7,18 +8,24 @@ namespace Project0.StoreApplication.Client
 {
     internal class Program
     {
-
         private const string _loggerFile = @"/home/christie/revature/christie_repo/data/logs.txt";
-        static void Main(string[] args)
+        private static readonly StoreSingleton _storeSingleton = StoreSingleton.Instance;
+
+        private static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration().WriteTo.File(_loggerFile).CreateLogger();
-            var p = new Program();
 
-            p.PrintRepoContent();
-            Console.WriteLine(p.SelectAStore());
+            runProgram();
         }
 
-        void PrintRepoContent()
+        private static void runProgram()
+        {
+          Log.Information("method: runProgram()");
+          PrintRepoContent();
+          Console.WriteLine(SelectAStore());
+        }
+
+        private static void PrintRepoContent()
         {
           Log.Information("method: PrintRepoContent()");
           var storeRepository = new StoreRepository();
@@ -31,7 +38,7 @@ namespace Project0.StoreApplication.Client
           }
         }
 
-        Bookstore SelectAStore()
+        private static Bookstore SelectAStore()
         {
           Log.Information("method: SelectAStore()");
           var sr = new StoreRepository().Bookstores;
---
+++ projects/project_0/Project0.StoreApplication.Client/Singletons/StoreSingleton.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Project0.StoreApplication.Domain.Models;
+using Project0.StoreApplication.Storage.Repositories;
+
+namespace Project0.StoreApplication.Client.Singletons
+{
+  /// <summary>
+  /// Singleton for Stores
+  /// </summary>
+  public class StoreSingleton
+  {
+    public List<Bookstore> Bookstores { get; }
+    private static StoreSingleton _storeSingleton;
+    private static readonly StoreRepository _storeRepository = new StoreRepository();
+    public static StoreSingleton Instance 
+    {
+      get
+      {
+        if (_storeSingleton == null)
+        {
+          _storeSingleton = new StoreSingleton();
+        }
+
+        return _storeSingleton;
+      }
+    }
+  }
+}
