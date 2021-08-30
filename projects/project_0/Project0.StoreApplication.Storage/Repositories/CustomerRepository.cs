4eb903ef76ce3538a8ce6acf0e025a128c4728c1
christie thammavong <christie.thammavong@revature.net>
2021-08-30 00:35:59 -0600

    modified file to correctly implement interface

--- projects/project_0/Project0.StoreApplication.Storage/Repositories/CustomerRepository.cs
+++ projects/project_0/Project0.StoreApplication.Storage/Repositories/CustomerRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Project0.StoreApplication.Domain.Interfaces;
 using Project0.StoreApplication.Domain.Models;
 using Project0.StoreApplication.Storage.Adapters;
 
@@ -7,29 +8,42 @@ namespace Project0.StoreApplication.Storage.Repositories
   /// <summary>
   /// Repository for Customers
   /// </summary>
-  public class CustomerRepository
+  public class CustomerRepository : IRepository<Customer>
   {
-    private const string path = @"/home/christie/revature/christie_repo/data/customers.xml";
-    public List<Customer> Customers
-    {
-      get;
-    }
+    private const string _path = @"/home/christie/revature/christie_repo/data/customers.xml";
+    private static readonly FileAdapter _fileAdapter = new FileAdapter();
 
     public CustomerRepository()
     {
-      var fileAdapter = new FileAdapter();
-
-      if (fileAdapter.ReadFromFile<Customer>(path) == null)
+      if (_fileAdapter.ReadFromFile<Customer>(_path) == null)
       {
-        fileAdapter.SaveToFile<Customer>(path, new List<Customer>()
+        _fileAdapter.SaveToFile<Customer>(_path, new List<Customer>()
         {
           new Customer() { firstName = "Tanjiro" , lastName = "Kamado"},
           new Customer() { firstName = "Naruto" , lastName = "Uzumaki"},
           new Customer() { firstName = "Kyo" , lastName = "Sohma" }
         });
       }
+    }
 
-      Customers = fileAdapter.ReadFromFile<Customer>(path);
+    public bool Insert()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Customer Update()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public List<Customer> Select()
+    {
+      return _fileAdapter.ReadFromFile<Customer>(_path);
+    }
+
+    public bool Delete()
+    {
+      throw new System.NotImplementedException();
     }
   }
 }
