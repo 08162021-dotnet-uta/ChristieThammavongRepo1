c66c8da03bf50a32123a565b33c1fe8a63666312
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-15 10:42:32 -0600

    EF Models were not in ModelsLayer for some reason

---
+++ projects/ModelsLayer/EFModels/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DbContextLayer.Models
+{
+    public partial class Book
+    {
+        public Book()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public byte BookId { get; set; }
+        public byte BookTypeId { get; set; }
+        public byte BookGenreId { get; set; }
+        public decimal BookPrice { get; set; }
+        public byte QuantityAvailable { get; set; }
+
+        public virtual BookGenre BookGenre { get; set; }
+        public virtual BookType BookType { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
