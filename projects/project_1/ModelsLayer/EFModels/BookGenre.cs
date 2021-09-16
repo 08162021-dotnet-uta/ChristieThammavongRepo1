78e585073e73a98b7d0a279569cd99310c4b513f
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-15 23:11:18 -0600

    moved files to correct place

---
+++ projects/project_1/ModelsLayer/EFModels/BookGenre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DbContextLayer.Models
+{
+    public partial class BookGenre
+    {
+        public BookGenre()
+        {
+            Books = new HashSet<Book>();
+        }
+
+        public byte BookGenreId { get; set; }
+        public string BookGenre1 { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
