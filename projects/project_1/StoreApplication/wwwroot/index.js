f89b0c50101c70de05caabc94396bb3ca47164e6
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-16 09:53:19 -0600

    completed landing page

--- projects/project_1/StoreApplication/wwwroot/index.js
+++ projects/project_1/StoreApplication/wwwroot/index.js
@@ -1,26 +1,11 @@
-const seecustomers = document.querySelector('.seecustomers');
+function CustomerLogin() {
+    location.href = "../HTML/loginCustomer.html";
+}
 
-seecustomers.addEventListener('click', (e) => {
-	fetch('Customer/ListOfCustomers') // Fetch() is default 'GET'
-		.then(res => {
-			if (!res.ok) {
-				console.log('NOT OK')
-				throw new Error(`Network response was not ok (${res.status})`);
-			}
-			else {
-				return res.json();
-			}
-		})
-		.then(res => {
-			console.log(res)
-			const loc = document.querySelector('.listofcustomers');
-			for (let x = 0; x < res.length; x++) {
-				loc.innerHTML += `<p>The customer is ${res[x].vFirstName} ${res[x].vLastName}.</p>`;
-			}
-		})
-		.catch(err => console.log(`There was an error ${err}`));
-});
+function StoreLogin() {
+    location.href = "../HTML/loginStore.html";
+}
 
-function SeeCustomers() {
-    location.href = "customer.html";
+function CustomerRegister() {
+    location.href = "../HTML/registerCustomer.html";
 }
