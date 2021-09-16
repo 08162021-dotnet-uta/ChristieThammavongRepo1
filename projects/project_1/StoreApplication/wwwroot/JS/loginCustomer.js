dad98cb3424c00481c379473cd1a038603595bee
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-16 09:53:47 -0600

    moved listofcustomer funtionality

--- projects/project_1/StoreApplication/wwwroot/JS/loginCustomer.js
+++ projects/project_1/StoreApplication/wwwroot/JS/loginCustomer.js
@@ -1,12 +1,22 @@
-﻿//we can call an IIFE to get a list of all the customers
-(function () {
-	fetch("customer/ListOfCustomers")
-		.then(res => res.json())
-		.then(data => {
-			console.log(data)
-			const lop = document.querySelector('.listofcustomers');
-			for (let x = 0; x < data.length; x++) {
-				lop.innerHTML += `<p>The customer is ${data[x].FirstName} ${data[x].LastName}.</p>`;
+﻿const listofcustomers = document.querySelector('.listofcustomers');
+
+listofcustomers.addEventListener('click', (e) => {
+	fetch('Customer/ListOfCustomers') // Fetch() is default 'GET'
+		.then(res => {
+			if (!res.ok) {
+				console.log('NOT OK')
+				throw new Error(`Network response was not ok (${res.status})`);
 			}
-		});
-})();
+			else {
+				return res.json();
+			}
+		})
+		.then(res => {
+			console.log(res)
+			const loc = document.querySelector('.listofcustomers');
+			for (let x = 0; x < res.length; x++) {
+				loc.innerHTML += `<p>The customer is ${res[x].vFirstName} ${res[x].vLastName}.</p>`;
+			}
+		})
+		.catch(err => console.log(`There was an error ${err}`));
+});
