bc5562cdab93844764664881a0f2020665b1bb1a
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-16 16:10:57 -0600

    completed customer login page

--- projects/project_1/StoreApplication/wwwroot/JS/loginCustomer.js
+++ projects/project_1/StoreApplication/wwwroot/JS/loginCustomer.js
@@ -1,4 +1,5 @@
 ﻿const listofcustomers = document.querySelector('#listofcustomers');
+const submit = document.querySelector('.submitbutton');
 
 (function () {
 	fetch("../Customer/ListOfCustomers")
@@ -7,30 +8,19 @@
 			console.log(data)
 			const loc = document.querySelector('#customers');
 			for (let x = 0; x < data.length; x++) {
-				/*lop.innerHTML += `<p>The customer is ${data[x].fname} ${data[x].lname}.</p>`;*/
 				loc.innerHTML += `<option value="${data[x].vFirstName} ${data[x].vLastName}">`;
 			}
 		})
-}) ();
+})();
 
-//listofcustomers.addEventListener('keyup', (e) => {
-//	fetch('../Customer/ListOfCustomers') // Fetch() is default 'GET'
-//		.then(res => {
-//			if (!res.ok) {
-//				console.log('NOT OK')
-//				throw new Error(`Network response was not ok (${res.status})`);
-//			}
-//			else {
-//				return res.json();
-//			}
-//		})
-//		.then(res => {
-//			console.log(res)
-//			const loc = document.querySelector('#customers');
-//			for (let x = 0; x < res.length; x++) {
-//				/*loc.innerHTML += `<p>The customer is ${res[x].vFirstName} ${res[x].vLastName}.</p>`;*/
-//				loc.innerHTML += `<option value="${res[x].vFirstName} ${res[x].vLastName}">`;
-//			}
-//		})
-//		.catch(err => console.log(`There was an error ${err}`));
-//});
+submit.addEventListener('click', (e) => {
+	const Value = document.querySelector('#listofcustomers').value;
+
+	if (Value == " ") {
+		alert("It seems you don't have an account. Please register.");
+		location.href = "../HTML/registerCustomer.html";
+	}
+	else {
+		location.href = "../HTML/customer.html";
+	}
+});
