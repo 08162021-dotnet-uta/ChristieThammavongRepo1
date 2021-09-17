330495fa9a2ee69a339aeea8bdef6958b42a78da
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-16 23:56:37 -0600

    modified registration page layout

--- projects/project_1/StoreApplication/wwwroot/JS/registerCustomer.js
+++ projects/project_1/StoreApplication/wwwroot/JS/registerCustomer.js
@@ -1,1 +1,43 @@
-﻿
+﻿/*const newcustomer = document.querySelector("#newcustomer");*/
+//const submitcustomer = document.querySelector(".submitbutton");
+
+//submitcustomer.addEventListener('submit', (e) => {
+//	e.preventDefault();
+//	const firstname = newcustomer.FirstName.value;
+//	const lastname = newcustomer.LastName.value;
+//	const userData = { VCustomerId: -1, VFirstName: firstname, VLastName: lastname }
+
+//	//GET fetch request
+//	fetch('../Customer/RegisterCustomer', {
+//		method: 'POST',
+//		headers: {
+//			'Accept': 'application/json',
+//			'Content-Type': 'application/json'
+//		},
+//		body: JSON.stringify(userData)
+//	})
+//		.then(response => {
+//			if (!response.ok) {
+//				throw new Error(`Network response was not ok (${response.status})`);
+//			}
+//			else       // When the page is loaded convert it to text
+//				return response.json();
+//		})
+//		.then((jsonResponse) => {
+//			//debugger;
+//			console.log(jsonResponse);
+//			//registerResponse.textContent = ` Welcome, ${jsonResponse.fname} ${jsonResponse.lname}`;
+//			//console.log(jsonResponse);
+//			//return jsonResponse;
+//		})
+//		//.then(res => {
+//		//	//save the personId to localStorage
+//		//	localStorage.setItem('person', JSON.stringify(res));// this is available to the whole browser
+//		//	//sessionStorage.setItem('personId', res);// this is ony vailable to the certain window tab.
+//		//	//switch the screen
+//		//	// location = 'personmenu.html';// 
+//		//})
+//		.catch(function (err) {
+//			console.log('Failed to fetch page: ', err);
+//		});
+//});
