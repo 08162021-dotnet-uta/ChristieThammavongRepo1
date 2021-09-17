bc7dd6e907a64939504d93a8d99aa92fb661a5ee
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-16 19:11:00 -0600

    modified files for store

--- projects/project_1/StoreApplication/wwwroot/JS/loginStore.js
+++ projects/project_1/StoreApplication/wwwroot/JS/loginStore.js
@@ -1,1 +1,17 @@
-﻿
+﻿const submit = document.querySelector('.submitbutton');
+
+(function () {
+	fetch("../Store/ListOfStores")
+		.then(res => res.json())
+		.then(data => {
+			console.log(data)
+			const los = document.querySelector('#stores');
+			for (let x = 0; x < data.length; x++) {
+				los.innerHTML += `<option value="${data[x].vLocation}">`;
+			}
+		})
+})();
+
+submit.addEventListener('click', (e) => {
+	location.href = "../HTML/store.html";
+});
