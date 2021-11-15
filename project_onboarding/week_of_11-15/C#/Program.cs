8a93baf3b21d1f445e43f00263ca9ab3b14d43fd
ChristieThammavongRevature <christie.thammavong@revature.net>
2021-11-15 14:14:04 -0700

    completed C#/.NET coding challenge

---
+++ project_onboarding/week_of_11-15/C#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        public static string ReverseCase(string phrase)
+        {
+          string reversed = "";
+          for(int i = 0; i < phrase.Length; i++)
+          {
+            if(char.IsUpper(phrase[i]))
+            {
+              reversed += char.ToLower(phrase[i]);
+            }
+            else if(char.IsLower(phrase[i]))
+            {
+              reversed += char.ToUpper(phrase[i]);
+            }
+            else if(phrase[i] == ' ')
+            {
+              reversed += phrase[i];
+            }
+          }
+          return reversed;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseCase("Happy Birthday")); // "hAPPY bIRTHDAY"
+
+            Console.WriteLine(ReverseCase("MANY THANKS")); // "many thanks"
+
+            Console.WriteLine(ReverseCase("sPoNtAnEoUs")); // "SpOnTaNeOuS"
+        }
+    }
+}
